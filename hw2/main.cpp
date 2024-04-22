abd36c0ce2ecc41ef99cf549c505ffea399e7205
Danya <144843893+yellowb3nz@users.noreply.github.com>
2024-04-22 13:29:59 +0200

    Add files via upload

---
+++ hw2/main.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cmath>
+
+// Создать класс, описывающий квадрат. Рассчитать в классе площадь, периметр и длину диагонали фигуры.
+
+class Square
+{
+private:
+	// Проверка на положительное значение длины стороны
+	bool isPositive()
+	{
+		if (side > 0)
+		{
+			return true;
+		}
+	}
+
+	void Area()
+	{
+		std::cout << "Площадь: " << side * side << std::endl;
+	}
+
+	void Perimeter()
+	{
+		std::cout << "Периметр: " << 4 * side << std::endl;
+	}
+
+	void Diagonal()
+	{
+		std::cout << "Диагональ: " << sqrt(2) * side << std::endl;
+	}
+
+public:
+	int side;
+
+	int Calculations()
+	{
+		if (isPositive())
+		{
+			Area();
+			Perimeter();
+			Diagonal();
+		}
+		else
+		{
+			std::cout << "Ошибка: Отрицательная длина.";
+			return 0;
+		}
+	}
+};
+
+int main()
+{
+	setlocale(LC_ALL, "ru");
+
+	float n;
+	std::cout << "Введите длину стороны квадрата: ";
+	std::cin >> n;
+	std::cout << std::endl;
+
+	Square exampleSquare;
+	exampleSquare.side = n;
+	exampleSquare.Calculations();
+
+	return 0;
+}
