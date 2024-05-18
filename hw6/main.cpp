7eb19be6e2252365bafbd9174690fd67d7e12c4c
Danya <144843893+yellowb3nz@users.noreply.github.com>
2024-05-18 11:16:53 +0200

    Add files via upload

---
+++ hw6/main.cpp
@@ -0,0 +1,62 @@
+﻿#include <iostream>
+#include "SFML/Graphics.hpp"
+
+int main() 
+{
+    sf::RenderWindow window(sf::VideoMode(1200, 630), "game");
+    sf::Texture spaceTexture;
+    sf::Texture ufoTexture;
+    sf::Sprite spriteSpaceTexture;
+    sf::Sprite spriteUFOTexture;
+
+    if (!spaceTexture.loadFromFile("assets\\space.jpg")) 
+    {
+        std::cout << "Error!!! space.jpg is not loading!" << std::endl;
+        return false;
+    }
+    if (!ufoTexture.loadFromFile("assets\\ufo.png")) 
+    {
+        std::cout << "Error!!! ufo.png is not loading!" << std::endl;
+        return false;
+    }
+
+    spriteSpaceTexture.setTexture(spaceTexture);
+    spriteUFOTexture.setTexture(ufoTexture);
+    spriteUFOTexture.setPosition(600, 315);
+    spriteUFOTexture.setScale(0.25f, 0.25f);
+
+    while (window.isOpen()) {
+        sf::Event event;
+        while (window.pollEvent(event)) 
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+        
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+        {
+            spriteUFOTexture.move(0.0f, -0.5f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+        {
+            spriteUFOTexture.move(0.0f, 0.5f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) 
+        {
+            spriteUFOTexture.move(-0.5f, 0.0f);
+        }
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) 
+        {
+            spriteUFOTexture.move(0.5f, 0.0f);
+        }
+
+        window.clear();
+
+        window.draw(spriteSpaceTexture);
+        window.draw(spriteUFOTexture);
+
+        window.display();
+    }
+
+    return 0;
+}
